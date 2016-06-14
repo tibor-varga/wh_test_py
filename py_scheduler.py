fef5cf59d3b53dd6cbead2cfd90390e299d2d924
Wade Hampton <wadehamptoniv@gmail.com>
2016-06-14 14:33:35 -0400

    updated template and several scripts

--- py_scheduler.py
+++ py_scheduler.py
@@ -7,27 +7,54 @@
 # 2) See notes about issues with threads....
 # 3) This is similar to the linked list timer Q I used
 #    in some test code (glib2-based).
+# 4) This is not recommended, recommended to use Timer
 #
+"""Schedulder example, recommended is to use Timer instead"""
+
+# Defines:
+num = 10  # number of events
+tmo = 2   # sec timeout
+
 import sched, time
 
 # create a new scheduler using real-time:
 #   time_func is time.time
 #   delay_func is time.sleep
 # -- these can be changed, e.g., for "simulated" time
-s = sched.scheduler(time.time, time.sleep)
+SCH = sched.scheduler(time.time, time.sleep)
 
 # event handler (callback)
-def print_time(): print "From print_time: ", time.time()
+def print_time():
+    """Print current time"""
+    print "From print_time: ", time.time()
 
-# function to run the test 
+# function to run the test
 def print_some_times():
+    """Print times used by the schedule"""
     print "From main:       ", time.time()
-    s.enter(5, 1, print_time, ())
-    s.enter(10, 1, print_time, ())
+    SCH.enter(5, 1, print_time, ())
+    SCH.enter(10, 1, print_time, ())
     # start it and run the events....
-    s.run()
+    SCH.run()
     print "From main:       ", time.time()
-    
+
+def pt_2():
+    """print times and reschedule"""
+    print_time()
+    global num
+    num = num - 1
+    if num > 0:
+        SCH.enter(tmo, 1, pt_2, ())
+
 # do the test
 print_some_times()
+
+# test of a periodic schedule
+print "Periodic test, num=%d, tmo=%d sec" % (num, tmo)
+
+#SCH_enter(tmo, 1, pt_2, ())
+pt_2()
+SCH.run()
+
 print "Done!"
+
--- thread_1.py
+++ thread_1.py
@@ -1,64 +1,67 @@
 #!/usr/bin/env python
 # Module:   thread_1.py
-# Purpose:  thread test 
+# Purpose:  thread test program
 # Date:     N/A
 # Notes:
 # 1) Reference:
 #     https://docs.python.org/3/library/threading.html
 #     https://pymotw.com/2/threading/
 #
+"""thread test program 1, Timer and Event"""
 import threading
 from threading import Timer
 from threading import Event
 import time as t
-td=10.0
+td = 10.0
 
 # callback function for timer, a sub-class of threads
 def hello():
-    te=t.asctime()
+    """callback function for timer, a sub-class of threads"""
+    te = t.asctime()
     evt.set()
     print te, ":  hello, world"
 
-# define a worker thread 
+# define a worker thread
 def my_thread():
-    tt=t.asctime()
+    """test worker thread"""
+    tt = t.asctime()
     print tt, ":  worker thread running"
     t.sleep(1)
     return
 
 # main below here....
-
 print "thread_1.py:  simple thread test program"
 
 # normal thread test
-th=threading.Thread(target=my_thread)
+th = threading.Thread(target=my_thread)
 
-tt=t.asctime()
+tt = t.asctime()
 print tt, ":  thread starting"
 th.start()
 
-tt=t.asctime()
+tt = t.asctime()
 print tt, ":  thread joining"
 th.join()
 
-tt=t.asctime()
+tt = t.asctime()
 print tt, ":  thread done"
+# end of normal thread test...
+
 
 # event for the timer test
 print "create test event and set to F"
-evt=Event()
+evt = Event()
 
 # timer test
 print "create a timer for ", td, " seconds"
-ts=t.asctime()
-tm= Timer(td, hello)
+ts = t.asctime()
+tm = Timer(td, hello)
 print ts, ":  starting...."
-tm.start() # after 30 seconds, "hello, world" will be printed
+tm.start() # after td seconds, "hello, world" will be printed
 
-tt=t.asctime()
+tt = t.asctime()
 print tt, ":  thread running -- waiting"
 
 evt.wait()
-tt=t.asctime()
+tt = t.asctime()
 print tt, ":  got event, exiting"
-
